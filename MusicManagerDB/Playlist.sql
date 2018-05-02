1a33c3f2d7cb19e5e8b111ff6e91fa1fdd06ce0c
EzuBotz <ezek_bajada@hotmail.co.uk>
2018-05-02 14:20:25 +0100

    Added table

---
+++ MusicManagerDB/Playlist.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Playlist]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NULL, 
+    [UserId] NCHAR(10) NULL, 
+    CONSTRAINT [FK_Playlist_ToTable] FOREIGN KEY ([Column]) REFERENCES [ToTable]([ToTableColumn])
+)
